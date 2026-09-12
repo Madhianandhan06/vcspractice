eb0ea257ee9c8e369f8500946ff793581b1e7d7c
Madhianandhan <152016565+Madhianandhan06@users.noreply.github.com>
2026-09-12 15:54:12 +0530

    feat: added modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import React, { useState } from 'react'
 const App = () => {
 
   const [count, setCount] = useState(1)
+  const [isOpen, setOpen] = useState(false)
   return (
     <div>
       <button onClick={() => setCount( c => c-1)}>-</button>
         <h3>{count}</h3>
       <button onClick={() => setCount( c => c+1)}>+</button>
+
+      <div>
+        <button onClick={() => setOpen(p => !p)}>{`Modal ${isOpen ? 'OFF' : 'ON'}`}</button>
+        {isOpen && <div>This is Modal</div>}
+      
+      </div>
     </div>
   )
 }
